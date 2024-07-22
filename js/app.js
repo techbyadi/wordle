2f312b391efdd03771e9882d79f5356ebc1e42fa
techbyadi <pssharma.aditya@gmail.com>
2024-07-22 15:13:26 -0700

    add compare function to compare words

--- js/app.js
+++ js/app.js
@@ -4,51 +4,78 @@ let words;
 
 /*--------- Variables (state) ----------*/
 
-words = ['', '', '', ''];
-let todaysWord = 'BEST';
-let index;
+
+let todaysWord = 'CARS';
+let index=0;
+let generatedWord;
 
 /*------- Cached Element References ------*/
 
 const rowElements = document.querySelectorAll('.sqr');
 const keyboardElements = document.querySelectorAll('.keyboard');
 const messageEl = document.getElementById('message');
+const resetEl = document.getElementById('reset');
 
 /*-------- Functions -----------------*/
 
+init();
+
+function init(){
+  words = ['', '', '', ''];
+  generatedWord = '';
+}
+
 function handleKeyboardClick(event){
-  let selectedWord = event.target.id;
-  placeWord(selectedWord);
-  displayWord();
-  console.log(`Words Array is: ${words}`);
+  let selectedLetter = event.target.id;
+  placeWord(selectedLetter);
+  displayWord(selectedLetter);
+  if(!words[3]==''){
+    compareWord();
+    init();
+  }
 }
 
-function displayWord(){
-    words.forEach((cell, idx) => {
-        rowElements[idx].textContent = cell;
-    })
+function compareWord(){
+  if(generatedWord === todaysWord){
+    console.log('Woahhhhh!!');
+  }
 }
 
-function placeWord(word){
+
+function displayWord(selectedLetter) {
+  if(index<20){
+  rowElements[index].textContent = selectedLetter;
+  console.log(index);
+  index++;
+  }
+  //console.log(`${element.textContent} is the element`);
+}
+
+function placeWord(selectedLetter){
     if(words[0]===''){
-      words[0] = word;
+      words[0] = selectedLetter;
     }
     else if(words[1]===''){
-      words[1] = word;
+      words[1] = selectedLetter;
     }
     else if(words[2]===''){
-      words[2] = word;
+      words[2] = selectedLetter;
     }
     else if(words[3]===''){
-      words[3] = word;
-    }
-
-  index = words.indexOf(word);  
-  return index;     
+      words[3] = selectedLetter;
+    } 
+    generatedWord = `${generatedWord}${selectedLetter}`;
+    console.log(generatedWord);      
 }
+
+function reset (){
+  render();
+}
+
 
 /*----------- Event Listeners ----------*/
 
 keyboardElements.forEach((keyboardElement)=> {
   keyboardElement.addEventListener('click', handleKeyboardClick);
 })
+resetEl.addEventListener('click', reset);
