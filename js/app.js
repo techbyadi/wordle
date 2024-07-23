b3ab9dbab05eb7b4b2c815183d5d0e94978157ac
techbyadi <pssharma.aditya@gmail.com>
2024-07-23 09:18:37 -0700

    add 6th row and 5th column to the board and update compare function

--- js/app.js
+++ js/app.js
@@ -3,13 +3,21 @@ let words;
 
 /*--------- Variables (state) ----------*/
 
-let todaysWord = "CARS";
+let todaysWord = "PROST";
 let index = 0;
 let generatedWord;
+let result = false;
+let rowIndex = 1;
 
 /*------- Cached Element References ------*/
 
-const rowElements = document.querySelectorAll(".sqr");
+const firstRowElements = document.querySelectorAll(".sqr1");
+const secondRowElements = document.querySelectorAll(".sqr2");
+const thirdRowElements = document.querySelectorAll(".sqr3");
+const fourthRowElements = document.querySelectorAll(".sqr4");
+const fifthRowElements = document.querySelectorAll(".sqr5");
+const sixthRowElements = document.querySelectorAll(".sqr6");
+const allElements = document.querySelectorAll("#row");
 const keyboardElements = document.querySelectorAll(".keyboard");
 const messageEl = document.getElementById("message");
 const resetEl = document.getElementById("reset");
@@ -19,7 +27,7 @@ const resetEl = document.getElementById("reset");
 init();
 
 function init() {
-  words = ["", "", "", ""];
+  words = ["", "", "", "",""];
   generatedWord = "";
 }
 
@@ -27,29 +35,85 @@ function handleKeyboardClick(event) {
   let selectedLetter = event.target.id;
   placeWord(selectedLetter);
   displayWord(selectedLetter);
-  if (!words[3] == "") {
+  if (!words[4] == "") {
     compareWords();
     init();
   }
 }
 
 function compareWords() {
+  if(rowIndex===1){
   for (let letter = 0; letter < todaysWord.length; letter++) {
     if (generatedWord[letter] === todaysWord[letter]) {
-      rowElements[letter].style.backgroundColor = "green";
+      firstRowElements[letter].style.backgroundColor = "green";
     } else if (todaysWord.includes(generatedWord[letter])) {
-      rowElements[letter].style.backgroundColor = "yellow";
+      firstRowElements[letter].style.backgroundColor = "yellow";
     } else {
-      rowElements[letter].style.backgroundColor = "gray";
+      firstRowElements[letter].style.backgroundColor = "gray";
+    }
+  }}
+  else if(rowIndex===2){
+    for (let letter = 0; letter < todaysWord.length; letter++) {
+      if (generatedWord[letter] === todaysWord[letter]) {
+        secondRowElements[letter].style.backgroundColor = "green";
+      } else if (todaysWord.includes(generatedWord[letter])) {
+        secondRowElements[letter].style.backgroundColor = "yellow";
+      } else {
+        secondRowElements[letter].style.backgroundColor = "gray";
+      }
     }
   }
+  else if(rowIndex===3){
+    for (let letter = 0; letter < todaysWord.length; letter++) {
+      if (generatedWord[letter] === todaysWord[letter]) {
+        thirdRowElements[letter].style.backgroundColor = "green";
+      } else if (todaysWord.includes(generatedWord[letter])) {
+        thirdRowElements[letter].style.backgroundColor = "yellow";
+      } else {
+        thirdRowElements[letter].style.backgroundColor = "gray";
+      }
+    }
+  }
+  else if(rowIndex ===4){
+    for (let letter = 0; letter < todaysWord.length; letter++) {
+      if (generatedWord[letter] === todaysWord[letter]) {
+        fourthRowElements[letter].style.backgroundColor = "green";
+      } else if (todaysWord.includes(generatedWord[letter])) {
+        fourthRowElements[letter].style.backgroundColor = "yellow";
+      } else {
+        fourthRowElements[letter].style.backgroundColor = "gray";
+      }
+    }
+  }
+  else if(rowIndex===5){
+    for (let letter = 0; letter < todaysWord.length; letter++) {
+      if (generatedWord[letter] === todaysWord[letter]) {
+        fifthRowElements[letter].style.backgroundColor = "green";
+      } else if (todaysWord.includes(generatedWord[letter])) {
+        fifthRowElements[letter].style.backgroundColor = "yellow";
+      } else {
+        fifthRowElements[letter].style.backgroundColor = "gray";
+      }
+    }
+  }
+  else if(rowIndex===6){
+    for (let letter = 0; letter < todaysWord.length; letter++) {
+      if (generatedWord[letter] === todaysWord[letter]) {
+        sixthRowElements[letter].style.backgroundColor = "green";
+      } else if (todaysWord.includes(generatedWord[letter])) {
+        sixthRowElements[letter].style.backgroundColor = "yellow";
+      } else {
+        sixthRowElements[letter].style.backgroundColor = "gray";
+      }
+    }
+  }
+  rowIndex++;
 }
 
 function displayWord(selectedLetter) {
-  if (index < 20) {
-    rowElements[index].textContent = selectedLetter;
-    rowElements[index].classList.add("animate__animated", "animate__fadeIn");
-    console.log(index);
+  if (index < 30) {
+    allElements[index].textContent = selectedLetter;
+    allElements[index].classList.add("animate__animated", "animate__fadeIn");
     index++;
   }
 }
@@ -63,9 +127,10 @@ function placeWord(selectedLetter) {
     words[2] = selectedLetter;
   } else if (words[3] === "") {
     words[3] = selectedLetter;
+  } else if (words[4] === "") {
+    words[4] = selectedLetter;
   }
   generatedWord = `${generatedWord}${selectedLetter}`;
-  console.log(generatedWord);
 }
 
 /*----------- Event Listeners ----------*/
