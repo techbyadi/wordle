4b9ebd4ec4d45ffde76b1d3654d7009102591407
techbyadi <pssharma.aditya@gmail.com>
2024-07-25 23:58:33 -0700

    update data.js file for words

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { checkWord, getWord, } from "./data.js";
 /*---------------- Constants ---------------*/
 
-let words;
+let letters;
 let validLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 /*--------- Variables (state) ----------*/
@@ -15,8 +15,6 @@ let rowIndex = 0;
 /*------- Cached Element References ------*/
 
 const rowElements = document.querySelectorAll(".row-elements");
-const allElements = document.querySelectorAll(".sqr");
-const keyboardEls = document.querySelectorAll(".rowKeys");
 const keyboardElements = document.querySelectorAll(".keyboard");
 const messageEl = document.getElementById("message");
 const resetEl = document.getElementById("reset");
@@ -28,11 +26,11 @@ init();
 function init() {
   console.log(`%cPsst... trying to cheat?`, `color: red; font-size: 25px; font-weight: bold;`);
   console.log(`%cThat's okay, we won't tell anyone. Here's the answer: ${todaysWord.toUpperCase()}`, `color: green; font-size: 16px; font-weight: bold;`);
-  setInitialValues();
+  render();
 }
 
-function setInitialValues(){
-  words = ["", "", "", "", ""];
+function render(){
+  letters = ["", "", "", "", ""];
   generatedWord = "";
   cursor = 0;
 }
@@ -49,7 +47,6 @@ function compareLetters() {
           "animate__animated",
           "animate__fadeInLeft"
         );
-        setTimeout(2000);
       } else if (todaysWord.includes(generatedWord[letter])) {
         rowElements[rowIndex].children[letter].style.backgroundColor =
           "rgb(201, 180, 88)";
@@ -59,7 +56,6 @@ function compareLetters() {
           "animate__animated",
           "animate__fadeInLeft"
         );
-        setTimeout(2000);
       } else {
         rowElements[rowIndex].children[letter].style.backgroundColor = "gray";
         document.getElementById(generatedWord[letter].toLowerCase()).style.backgroundColor =
@@ -68,7 +64,6 @@ function compareLetters() {
           "animate__animated",
           "animate__fadeInLeft"
         );
-        setTimeout(2000);
       }
     } else {
       rowElements[rowIndex].children[letter].classList.remove(
@@ -79,9 +74,9 @@ function compareLetters() {
         "animate__animated",
         "animate__shakeX"
       );
-      
     }
   }
+
   if (generatedWord === todaysWord) {
     result = true;
     confetti.start(1500);
@@ -106,42 +101,42 @@ function displayLetter(selectedLetter) {
   if (selectedLetter === "ENT" && !result && cursor === 5 && rowIndex < 6) {
     compareLetters();
       if (checkWord(generatedWord.toLowerCase())) {
-        setInitialValues();
+        render();
       }
   }
 
   if (!result) {
     if (validLetters.includes(selectedLetter)) {
-      if (words[0] === "") {
-        words[0] = selectedLetter;
+      if (letters[0] === "") {
+        letters[0] = selectedLetter;
         rowElements[rowIndex].children[cursor].textContent = selectedLetter;
         rowElements[rowIndex].children[cursor].classList.add(
           "animate__animated",
           "animate__fadeIn"
         );
-      } else if (words[1] === "") {
-        words[1] = selectedLetter;
+      } else if (letters[1] === "") {
+        letters[1] = selectedLetter;
         rowElements[rowIndex].children[cursor].textContent = selectedLetter;
         rowElements[rowIndex].children[cursor].classList.add(
           "animate__animated",
           "animate__fadeIn"
         );
-      } else if (words[2] === "") {
-        words[2] = selectedLetter;
+      } else if (letters[2] === "") {
+        letters[2] = selectedLetter;
         rowElements[rowIndex].children[cursor].textContent = selectedLetter;
         rowElements[rowIndex].children[cursor].classList.add(
           "animate__animated",
           "animate__fadeIn"
         );
-      } else if (words[3] === "") {
-        words[3] = selectedLetter;
+      } else if (letters[3] === "") {
+        letters[3] = selectedLetter;
         rowElements[rowIndex].children[cursor].textContent = selectedLetter;
         rowElements[rowIndex].children[cursor].classList.add(
           "animate__animated",
           "animate__fadeIn"
         );
-      } else if (words[4] === "") {
-        words[4] = selectedLetter;
+      } else if (letters[4] === "") {
+        letters[4] = selectedLetter;
         rowElements[rowIndex].children[cursor].textContent = selectedLetter;
         rowElements[rowIndex].children[cursor].classList.add(
           "animate__animated",
@@ -149,7 +144,7 @@ function displayLetter(selectedLetter) {
         );
       }
      
-      if (generatedWord.length >= 0 && generatedWord.length < words.length) {
+      if (generatedWord.length >= 0 && generatedWord.length < letters.length) {
         generatedWord = `${generatedWord}${selectedLetter}`;
         cursor++;
       }
@@ -162,7 +157,7 @@ function displayLetter(selectedLetter) {
 function deleteLetter() {
   if (cursor >= 1) {
     rowElements[rowIndex].children[cursor - 1].textContent = "";
-    words[cursor - 1] = "";
+    letters[cursor - 1] = "";
     generatedWord = generatedWord.slice(0, generatedWord.length - 1);
     cursor--;
   }
@@ -180,7 +175,7 @@ function handleKeys(evt){
   if (evt.key === "Enter" && !result && cursor === 5 && rowIndex < 6) {
     compareLetters();
     if (checkWord(generatedWord.toLowerCase())) {
-      setInitialValues();
+      render();
     }
   }
 
@@ -188,6 +183,8 @@ function handleKeys(evt){
     deleteLetter();
   }
 }
+
+
 /*----------- Event Listeners ----------*/
 
 keyboardElements.forEach((keyboardElement) => {
